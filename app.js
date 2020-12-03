0c050aaa243016f18b6fc231473813857fb715d6
Michael Griffith <griffithcmichael@gmail.com>
2020-12-03 16:26:47 -0600

    commented out

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { viewAllRoles } = require("./lib/viewAllRoles");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 3301,
+    port: 3306,
     user: "root",
     password:"rootroot",
     database: "employee_databaseDB"
