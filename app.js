65421f5f108c1662606cba662fc01ad363bdebc1
Michael Griffith <griffithcmichael@gmail.com>
2020-12-03 15:50:17 -0600

    set connection and started switch start

---
+++ app.js
@@ -0,0 +1,66 @@
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3301,
+    user: "root",
+    password:"rootroot",
+    database: "employee_databaseDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as" + connection.threadID)
+});
+
+function startPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees?",
+                "View All Employees By Departments?",
+                "View All Employess by Roles?",
+                "Update an Employee?",
+                "Add an Employee?",
+                "Add a Department?",
+                "Ass a Role?"
+            ]}
+
+            ]).then(function(val) {
+                switch (val.choice){
+                    case "View All Employees?":
+                    console.log("View All Employees?")
+                    break;
+
+                    case "View All Employees By Departments?":
+                    console.log("View All Employees By Departments?")
+                    break;
+
+                    case "View All Employess by Roles?":
+                    console.log("View All Employess by Roles?")
+                    break;
+
+                    case "Update an Employee?":
+                    console.log("Update an Employee?")
+                    break;
+
+                    case "Add an Employee?":
+                    console.log("Add an Employee?")
+                    break;
+
+                    case "Add a Department?":
+                    console.log("Add a Department?")
+                    break;
+
+                    case "Ass a Role?":
+                    console.log("Ass a Role?")
+                    break;
+                }
+            })
+        }
+
